bc77b03307fdbd6ded6771d63f0bf107c2a515f6
William Evans <will.evans138@gmail.com>
2023-04-19 17:09:03 -0500

    Added card value functionality to gameview

--- prototype/Pages/GamePage.xaml.cs
+++ prototype/Pages/GamePage.xaml.cs
@@ -11,11 +11,38 @@ public partial class GamePage : ContentPage
 		InitializeComponent();
 		hand = new ObservableCollection<Card>()
 		{
-			new Card(Card.Ranks.Three, Card.Suits.Spades),
+            new Card(Card.Ranks.Ace, Card.Suits.Hearts),
+            new Card(Card.Ranks.Three, Card.Suits.Spades),
 			new Card(Card.Ranks.Four, Card.Suits.Hearts),
         };
 
         DealerHand.ItemsSource = hand;
 		PlayerHand.ItemsSource = hand;
+
+		ValueOfDealersHand.Text = "Dealer's Hand: " + ValueOfHand(hand).ToString();
+		ValueOfPlayersHand.Text = "Player's Hand: " + ValueOfHand(hand).ToString();
     }
+	private int ValueOfHand(ObservableCollection<Card> hand)
+	{
+		int sum = 0;
+		int amountOfAces = 0;
+
+		foreach (Card card in hand)
+		{
+			if (card.Rank != Card.Ranks.Ace)
+				sum += card.Value;
+			else
+				amountOfAces += 1;
+		}
+
+		for (int i = 0; i < amountOfAces; i++)
+		{
+			if (sum + 11 <= 21)
+				sum += 11;
+			else
+				sum += 1;
+		}
+
+		return sum;
+	}
 }
