5583cee7dbf1dd74d5a39102e08f3326f4f80dc6
William Evans <will.evans138@gmail.com>
2023-04-27 18:15:27 -0500

    added player class (both for dealer and person player), and standing/hitting functionality

---
+++ prototype/Classes/Player.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prototype.Classes
+{
+    internal class Player
+    {
+        int Money { get; set; }
+        public ObservableCollection<Card> Hand { get; set; }
+        public int HandValue 
+        { 
+            get
+            {
+                return ValueOfHand(this.Hand);
+            }
+            private set
+            {
+
+            }
+        }
+
+        public Player()
+        {
+            Money = 1000;
+            Hand = new ObservableCollection<Card>();
+            HandValue = ValueOfHand(Hand);
+        }
+
+        private int ValueOfHand(ObservableCollection<Card> hand)
+        {
+            int sum = 0;
+            int amountOfAces = 0;
+
+            if (hand.Count > 0)
+            {
+                foreach (Card card in hand)
+                {
+                    if (card.Rank != Card.Ranks.Ace)
+                        sum += card.Value;
+                    else
+                        amountOfAces += 1;
+                }
+
+                for (int i = 0; i < amountOfAces; i++)
+                {
+                    if (sum + 11 <= 21)
+                        sum += 11;
+                    else
+                        sum += 1;
+                }
+
+                return sum;
+            }
+
+            return 0;
+        }
+    }
+}
